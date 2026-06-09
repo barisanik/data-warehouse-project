a138086aaedce83f8ddb583213f52f519176c390
Baris Anik <barisanik@gmail.com>
2026-06-09 23:46:59 +0300

    Add stg_djapi__product model with schema tests.
    These files replace the silver.djapi_product part of the proc_load_silver_data.sql file. Running the stg_djapi__product model will load data into the silver.stg_djapi__product table.

---
+++ dwh_dbt_files/models/staging/stg_djapi__product.sql
@@ -0,0 +1,40 @@
+/*
+	# ============================================================================ #
+		Model: Staging API Product Info
+	# ============================================================================ #
+
+    Script Purpose: This script loads data into the silver layer utilizing source tables of bronze layer.
+
+    Transformation processes:
+    - Removal of prefix and whitespaces for product id (id).
+    - Initial capitilization, whitespace removal and null replacement for title and category.
+    - Removal of suffix for product key (pkey).
+
+    Run Command: dbt run --select stg_djapi__product
+    Test Command: dbt test --select stg_djapi__product
+
+    WARNING:
+    - Materialization is set to 'table'. dbt will recreate 'silver.stg_djapi__product' on each run.
+*/
+
+WITH source AS (
+    SELECT * FROM {{ source('bronze_api', 'djapi_product') }}
+),
+cleaned AS(
+    SELECT
+        CAST(TRIM(REPLACE(id,'dummy-','')) AS INT) AS id -- Clear prefix.
+        ,[dbo].[FN_InitCap](TRIM(title)) AS title -- Set first character of each word uppercase.
+        ,[dbo].[FN_InitCap](TRIM(REPLACE(ISNULL(category,'n/a'),'-',' '))) AS category -- Set first character of each word uppercase.
+        ,CASE 
+            WHEN LEN(pkey) > 15 THEN SUBSTRING(UPPER(REPLACE(TRIM(pkey),'_','-')), 0, 16) -- Clear suffix
+            ELSE UPPER(REPLACE(TRIM(pkey),'_','-')) 
+        END AS pkey
+        ,createdAt
+        ,GETDATE() AS dwh_create_date
+    FROM
+        source
+    WHERE	
+        title IS NOT NULL -- Avoid nameless products
+)
+
+SELECT * FROM cleaned
